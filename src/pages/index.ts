5e233dc93c162e4b3ccedbc55a4621ee3ca00d5b
abhikachroo <66299522+abhikachroo@users.noreply.github.com>
2026-04-15 15:29:34 +0200

    feat(qe-ai): export new pages in barrel

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,5 @@
-export { BasePage }   from './BasePage';
-export { SearchPage } from './SearchPage';
+export { BasePage }           from './BasePage';
+export { SearchPage }         from './SearchPage';
+export { ProductDetailPage }  from './ProductDetailPage';
+export { CartPage }           from './CartPage';
+export { CheckoutPage }       from './CheckoutPage';
